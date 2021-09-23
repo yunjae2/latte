4bbf64c1bf16547dd89d674a11e1f6f9d2be2e67
Yunjae Lee <lyj7694@gmail.com>
2021-09-24 00:21:33 +0900

    controller/backend: Start backend project
    Signed-off-by: Yunjae Lee <lyj7694@gmail.com>

---
+++ controller/backend/src/main/java/com/latte/controller/LatteControllerApplication.java
@@ -0,0 +1,13 @@
+package com.latte.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatteControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LatteControllerApplication.class, args);
+    }
+
+}
---
+++ controller/backend/src/test/java/com/latte/controller/LatteControllerApplicationTest.java
@@ -0,0 +1,13 @@
+package com.latte.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatteControllerApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
