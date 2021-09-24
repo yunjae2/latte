5a31426f8562e26fee26c861e1d03cd17612d6f7
Yunjae Lee <lyj7694@gmail.com>
2021-09-24 15:13:00 +0900

    worker/api: Start worker api project
    Signed-off-by: Yunjae Lee <lyj7694@gmail.com>

---
+++ worker/api/src/main/java/com/latte/worker/LatteWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.latte.worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatteWorkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LatteWorkerApplication.class, args);
+    }
+
+}
---
+++ worker/api/src/test/java/com/latte/worker/LatteWorkerApplicationTest.java
@@ -0,0 +1,13 @@
+package com.latte.worker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatteWorkerApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
