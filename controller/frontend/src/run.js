0c566587df3f0db674f65330d2bf19757e42399b
Yunjae Lee <lyj7694@gmail.com>
2021-09-27 11:49:00 +0900

    controller/frontend: Replace script code with shell output
    Signed-off-by: Yunjae Lee <lyj7694@gmail.com>

--- controller/frontend/src/run.js
+++ controller/frontend/src/run.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid'
@@ -7,83 +6,7 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
-
-const demoScript = `import http from 'k6/http';
-import { URLSearchParams } from 'https://jslib.k6.io/url/1.0.0/index.js';
-import { check } from 'k6';
-
-const andKeywords = open("/home/k6/resources/and_keywords.txt").split("\n").filter(k => k)
-const orKeywords = open("/home/k6/resources/or_keywords.txt").split("\n").filter(k => k)
-
-/*
- * Utility methods
- */
-function getRandomInt(max) {
-	return Math.floor(Math.random() * max)
-}
-
-function getRandomItem(items) {
-	return items[getRandomInt(items.length)]
-}
-
-/*
- * Helper methods
- */
-function getKeyword() {
-	const AND_THRESHOLD = 92
-
-	if (getRandomInt(100) < AND_THRESHOLD)
-		return getRandomItem(andKeywords)
-	else
-		return getRandomItem(orKeywords)
-}
-
-function buildAddress() {
-	const url = "http://internal-sp-baemin-search-testing-6d42-459688424.ap-northeast-2.elb.amazonaws.com/shops/search"
-	const params = {
-		sortSet: "DEFAULT",
-		latitude: "37.51690682",
-		longitude: "127.1127602",
-		subRegionId4User: "11710562",
-		keyword: getKeyword(),
-		filters: "DEFAULT",
-	}
-
-	const searchParams = new URLSearchParams(params)
-
-	return url + "?" + searchParams
-}
-
-/*
- * k6 option and callback
- */
-export let options = {
-	discardResponseBodies: true,
-	scenarios: {
-		contacts: {
-			executor: 'constant-arrival-rate',
-			// TODO: use argument for the constant rate
-			rate: 1000,
-			// TODO: use argument for the duration
-			duration: '3m',
-			// TODO: use argument for the VUs
-			preAllocatedVUs: 200,
-			maxVUs: 1000,
-		},
-	},
-	summaryTrendStats: ['avg', 'min', 'med', 'max', 'p(50)', 'p(99)', 'p(99.9)', 'p(99.99)', 'p(100)', 'count'],
-	thresholds: {
-		http_req_failed: ['rate < 0.01'],
-		// TODO: use argument for the latency threshold
-		http_req_duration: ['p(99.99) < 700']
-	}
-}
-
-export default function() {
-	let res = http.get(buildAddress())
-	check(res, { 'status was 200': (r) => r.status == 200});
-}
-`;
+import { CssBaseline } from '@mui/material';
 
 export default function Run() {
 	const name = React.useRef();
@@ -111,8 +34,9 @@ export default function Run() {
 
     return (
         <React.Fragment>
-            <CssBaseline />
+			<CssBaseline />
             <Container maxWidth="false">
+				<Box sx={{ height: 30 }} />
                 <Box sx={{ height: 100 }}>
                     <Grid container spacing={4} alignItems="center">
                         <Grid item sm={3} md={3}>
@@ -129,11 +53,8 @@ export default function Run() {
                         </Grid>
                     </Grid>
                 </Box>
-				<div style={{ whiteSpace: "pre", fontFamily: "monospace", fontSize: 14 }}>
+                <SyntaxHighlighter language="shell" style={atomOneDark} codeTagProps={{style: {fontSize: 16}}}>
 					{output}
-				</div>
-                <SyntaxHighlighter showLineNumbers language="javascript" style={atomOneDark}>
-                    {demoScript}
                 </SyntaxHighlighter>
             </Container>
         </React.Fragment>
