df0ab8d4d3073ede582869bcc85c158e67f6de7d
Yunjae Lee <lyj7694@gmail.com>
2021-09-28 03:48:00 +0900

    controller/frontend: Disable run button during run
    Signed-off-by: Yunjae Lee <lyj7694@gmail.com>

--- controller/frontend/src/views/run/run.js
+++ controller/frontend/src/views/run/run.js
@@ -3,38 +3,41 @@ import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid'
 import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import LoadingButton from '@mui/lab/LoadingButton';
 
 export default function Run() {
-	const name = React.useRef();
-	const branch = React.useRef();
-	const script = React.useRef();
-	const [ output, setOutput ] = React.useState("");
+    const name = React.useRef();
+    const branch = React.useRef();
+    const script = React.useRef();
+    const [output, setOutput] = React.useState("");
+    const [loading, setLoading] = React.useState(false);
 
-	const requestRun = () => {
-		setOutput("")
-		let url = new URL("http://localhost:8080/run");
-		url.searchParams.set("testName", name.current.value);
-		url.searchParams.set("branchName", branch.current.value);
-		url.searchParams.set("scriptFilePath", script.current.value);
+    const requestRun = () => {
+        setOutput("");
+        setLoading(true);
+        let url = new URL("http://localhost:8080/run");
+        url.searchParams.set("testName", name.current.value);
+        url.searchParams.set("branchName", branch.current.value);
+        url.searchParams.set("scriptFilePath", script.current.value);
 
-		let eventSource = new EventSource(url.toString())
+        let eventSource = new EventSource(url.toString())
 
-		eventSource.onmessage = e => {
-			setOutput(output => output + e.data);
-		};
+        eventSource.onmessage = e => {
+            setOutput(output => output + e.data);
+        };
 
-		eventSource.onerror = e => {
-			eventSource.close();
-		}
-	}
+        eventSource.onerror = e => {
+            eventSource.close();
+            setLoading(false);
+        }
+    }
 
     return (
         <React.Fragment>
             <Container maxWidth="false">
-				<Box sx={{ height: 20 }} />
+                <Box sx={{ height: 20 }} />
                 <Box sx={{ height: 100 }}>
                     <Grid container spacing={4} alignItems="center">
                         <Grid item sm={3} md={3}>
@@ -47,12 +50,12 @@ export default function Run() {
                             <TextField fullWidth inputRef={script} id="script" label="Script file" variant="outlined" />
                         </Grid>
                         <Grid item sm={2} sm={1}>
-                            <Button onClick={requestRun} variant="contained" size="medium">Run</Button>
+                            <LoadingButton onClick={requestRun} loading={loading} variant="contained" size="medium">Run</LoadingButton>
                         </Grid>
                     </Grid>
                 </Box>
-                <SyntaxHighlighter language="shell" style={atomOneDark} codeTagProps={{style: {fontSize: 16}}}>
-					{output}
+                <SyntaxHighlighter language="shell" style={atomOneDark} codeTagProps={{ style: { fontSize: 16 } }}>
+                    {output}
                 </SyntaxHighlighter>
             </Container>
         </React.Fragment>
