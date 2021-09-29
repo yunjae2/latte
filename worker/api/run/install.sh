2bad3e0662f1ac315072a6470055541f5e432ed9
Yunjae Lee <lyj7694@gmail.com>
2021-09-29 23:17:23 +0900

    worker/api: Remove intermediate file
    Signed-off-by: Yunjae Lee <lyj7694@gmail.com>

--- worker/api/run/install.sh
+++ worker/api/run/install.sh
@@ -6,6 +6,7 @@ cd ../ &&
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     wget https://github.com/grafana/k6/releases/download/v0.34.1/k6-v0.34.1-linux-amd64.rpm &&
     sudo yum -y install k6-v0.34.1-linux-amd64.rpm
+    rm k6-v0.34.1-linux-amd64.rpm
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     brew install k6
 fi
