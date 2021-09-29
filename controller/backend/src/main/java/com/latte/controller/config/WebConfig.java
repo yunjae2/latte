4ef890696edb9d6f920681cea1d4bf620a1d38c3
Yunjae Lee <lyj7694@gmail.com>
2021-09-29 19:17:47 +0900

    con/be: Allow ip4 address as well as "localhost"

--- controller/backend/src/main/java/com/latte/controller/config/WebConfig.java
+++ controller/backend/src/main/java/com/latte/controller/config/WebConfig.java
@@ -1,18 +1,33 @@
 package com.latte.controller.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Slf4j
 @Configuration
 @EnableWebFlux
 public class WebConfig implements WebFluxConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        String hostAddress;
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            log.error("Failed to get host address", e);
+            throw new IllegalStateException(e);
+        }
+
         registry.addMapping("/**")
+                .allowedOrigins()
                 .allowedMethods("*")
-                .allowedOriginPatterns("http://localhost*");
+                .allowedOriginPatterns("http://localhost*", "http://" + hostAddress + "*");
     }
 }
