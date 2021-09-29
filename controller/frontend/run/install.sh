525eb692381e6115aadee14b021325af3a9042e4
Yunjae Lee <lyj7694@gmail.com>
2021-09-29 13:52:54 +0900

    con/fe: Open 80 port permission in Linux
    Signed-off-by: Yunjae Lee <lyj7694@gmail.com>

--- controller/frontend/run/install.sh
+++ controller/frontend/run/install.sh
@@ -6,6 +6,10 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash 
 source ~/.nvm/nvm.sh &&
 nvm install node &&
 
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    sudo setcap cap_net_bind_service=+ep $(which node)
+fi
+
 npm install &&
 npm run build &&
 npm install -g serve
