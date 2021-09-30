757bb5487d7b146259dd4395aed44aa8f9667299
Yunjae Lee <lyj7694@gmail.com>
2021-09-30 22:57:56 +0900

    con/fe: Implement sorting in history page
    Signed-off-by: Yunjae Lee <lyj7694@gmail.com>

--- controller/frontend/src/views/history/table.js
+++ controller/frontend/src/views/history/table.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Collapse from '@mui/material/Collapse';
-import IconButton from '@mui/material/IconButton';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -10,14 +9,93 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import { Label, LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
-import { TablePagination } from '@mui/material';
+import { TablePagination, Checkbox, TableSortLabel } from '@mui/material';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
 
 const grayCell = {
     backgroundColor: "F0F0F0"
 }
+
+function descendingComparator(a, b, orderBy) {
+    if (_.get(a, orderBy) < _.get(b, orderBy)) {
+        return -1;
+    }
+    if (_.get(a, orderBy) > _.get(b, orderBy)) {
+        return 1;
+    }
+    return 0;
+}
+
+function getComparator(order, orderBy) {
+    return order === 'desc'
+        ? (a, b) => descendingComparator(a, b, orderBy)
+        : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+
+function EnhancedTableCell(props) {
+    const { id, align, rowSpan, colSpan, orderBy, order, label, createSortHandler, style } = props;
+    return (
+        <TableCell
+            key={id}
+            align={align}
+            rowSpan={rowSpan}
+            colSpan={colSpan}
+            sortDirection={orderBy === id ? order : false}
+            style={style}
+        >
+            {label}
+            <br/>
+            <TableSortLabel
+                active={orderBy === id}
+                direction={orderBy === id ? order : 'asc'}
+                onClick={createSortHandler(id)}
+            >
+            </TableSortLabel>
+        </TableCell>);
+}
+
+function EnhancedTableHead(props) {
+    const { order, orderBy, onRequestSort } =
+        props;
+
+    const createSortHandler = (property) => (event) => {
+        onRequestSort(event, property);
+    };
+
+    return (
+        <TableHead>
+            <TableRow>
+                <TableCell rowSpan={2} />
+                <EnhancedTableCell id="name" align="left" label="Name" rowSpan={2} colSpan={2} orderBy={orderBy} order={order} createSortHandler={createSortHandler} />
+                <EnhancedTableCell id="date" align="center" label="Date" rowSpan={2} orderBy={orderBy} order={order} createSortHandler={createSortHandler} />
+                <EnhancedTableCell id="rps" align="right" label="RPS" rowSpan={2} orderBy={orderBy} order={order} createSortHandler={createSortHandler} />
+                <EnhancedTableCell id="duration" align="right" label="Duration" rowSpan={2} orderBy={orderBy} order={order} createSortHandler={createSortHandler} />
+                <TableCell colSpan={7} align="center" size="small">Latency&nbsp;(ms)</TableCell>
+            </TableRow>
+            <TableRow>
+                <EnhancedTableCell id="latency.min" align="right" label="min" rowSpan={1} orderBy={orderBy} order={order} createSortHandler={createSortHandler} style={grayCell} />
+                <EnhancedTableCell id="latency.avg" align="right" label="avg" rowSpan={1} orderBy={orderBy} order={order} createSortHandler={createSortHandler} style={grayCell} />
+                <EnhancedTableCell id="latency.max" align="right" label="max" rowSpan={1} orderBy={orderBy} order={order} createSortHandler={createSortHandler} style={grayCell} />
+                <EnhancedTableCell id="latency.p50" align="right" label="p50" rowSpan={1} orderBy={orderBy} order={order} createSortHandler={createSortHandler} />
+                <EnhancedTableCell id="latency.p99" align="right" label="p99" rowSpan={1} orderBy={orderBy} order={order} createSortHandler={createSortHandler} />
+                <EnhancedTableCell id="latency.p99_9" align="right" label="p99.9" rowSpan={1} orderBy={orderBy} order={order} createSortHandler={createSortHandler} />
+                <EnhancedTableCell id="latency.p99_99" align="right" label="p99.99" rowSpan={1} orderBy={orderBy} order={order} createSortHandler={createSortHandler} />
+            </TableRow>
+        </TableHead>
+    );
+}
+
+EnhancedTableHead.propTypes = {
+    numSelected: PropTypes.number.isRequired,
+    onRequestSort: PropTypes.func.isRequired,
+    onSelectAllClick: PropTypes.func.isRequired,
+    order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+    orderBy: PropTypes.string.isRequired,
+    rowCount: PropTypes.number.isRequired,
+};
 
 /* TODO: show VU stats */
 function convertToChartData(latency) {
@@ -43,24 +121,20 @@ function Row(props) {
     const { row } = props;
     const [open, setOpen] = React.useState(false);
 
+    const handleRowClick = () => {
+        setOpen((prevOpen) => !prevOpen);
+    }
+
     return (
         <React.Fragment>
-            <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
-                <TableCell>
-                    <IconButton
-                        aria-label="expand row"
-                        size="small"
-                        onClick={() => setOpen(!open)}
-                    >
-                        {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-                    </IconButton>
-                </TableCell>
-                <TableCell component="th" scope="row">
+            <TableRow sx={{ '& > *': { borderBottom: 'unset' } }} onClick={handleRowClick}>
+                <TableCell align="right" />
+                <TableCell colSpan={2}>
                     {row.name}
                 </TableCell>
                 <TableCell align="center">{row.date}</TableCell>
-                <TableCell align="center">{row.rps.toFixed(1)}</TableCell>
-                <TableCell align="center">{(row.duration / 1000).toFixed(0)}</TableCell>
+                <TableCell align="right">{row.rps.toFixed(1)}</TableCell>
+                <TableCell align="right">{(row.duration / 1000).toFixed(0)}</TableCell>
                 <TableCell align="right" style={grayCell}>{row.latency.min.toFixed(0)}</TableCell>
                 <TableCell align="right" style={grayCell}>{row.latency.avg.toFixed(0)}</TableCell>
                 <TableCell align="right" style={grayCell}>{row.latency.max.toFixed(0)}</TableCell>
@@ -95,11 +169,19 @@ function Row(props) {
     );
 }
 
-/* TODO: paging */
 export default function CollapsibleTable(props) {
     const { rows } = props;
-    const [ page, setPage ] = React.useState(0);
-    const [ rowsPerPage, setRowsPerPage ] = React.useState(10);
+    const [order, setOrder] = React.useState('asc');
+    const [orderBy, setOrderBy] = React.useState('calories');
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+    const handleRequestSort = (event, property) => {
+        const isAsc = orderBy === property && order === 'asc';
+        setOrder(isAsc ? 'desc' : 'asc');
+        setOrderBy(property);
+        setPage(0);
+    };
 
     const handlePageChange = (event, newPage) => {
         setPage(newPage);
@@ -114,29 +196,17 @@ export default function CollapsibleTable(props) {
         <React.Fragment>
             <TableContainer component={Paper}>
                 <Table aria-label="collapsible table" size="small">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell rowSpan={2} />
-                            <TableCell rowSpan={2} align="center">Name</TableCell>
-                            <TableCell rowSpan={2} align="center">Date</TableCell>
-                            <TableCell rowSpan={2} align="center">RPS</TableCell>
-                            <TableCell rowSpan={2} align="center">Duration&nbsp;(s)</TableCell>
-                            <TableCell colSpan={7} align="center" size="small">Latency&nbsp;(ms)</TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell align="right" size="small" style={grayCell}>min</TableCell>
-                            <TableCell align="right" size="small" style={grayCell}>avg</TableCell>
-                            <TableCell align="right" size="small" style={grayCell}>max</TableCell>
-                            <TableCell align="right" size="small">p50</TableCell>
-                            <TableCell align="right" size="small">p99</TableCell>
-                            <TableCell align="right" size="small">p99.9</TableCell>
-                            <TableCell align="right" size="small">p99.99</TableCell>
-                        </TableRow>
-                    </TableHead>
+                    <EnhancedTableHead
+                        order={order}
+                        orderBy={orderBy}
+                        onRequestSort={handleRequestSort}
+                        rowCount={rows.length}
+                    />
                     <TableBody>
-                        {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                        {rows.sort(getComparator(order, orderBy))
+                            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                             .map((row) => (
-                                <Row key={row.id} row={row} />
+                        <Row key={row.id} row={row} />
                             ))}
                     </TableBody>
                     <TablePagination
