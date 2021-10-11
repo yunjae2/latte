e1f84e97056c190ca9e6f67c92fe2ab8458dc2c9
Yunjae Lee <lyj7694@gmail.com>
2021-10-11 20:16:54 +0900

    con/be: Refactoring; extract config repository
    Signed-off-by: Yunjae Lee <lyj7694@gmail.com>

---
+++ controller/backend/src/main/java/com/latte/controller/repository/ConfigRepository.java
@@ -0,0 +1,47 @@
+package com.latte.controller.repository;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.latte.controller.config.ControllerConfig;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.endpoint.RefreshEndpoint;
+import org.springframework.stereotype.Repository;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static com.fasterxml.jackson.databind.SerializationFeature.WRAP_ROOT_VALUE;
+import static com.fasterxml.jackson.dataformat.yaml.YAMLGenerator.Feature.MINIMIZE_QUOTES;
+import static com.fasterxml.jackson.dataformat.yaml.YAMLGenerator.Feature.WRITE_DOC_START_MARKER;
+
+@Slf4j
+@RequiredArgsConstructor
+@Repository
+public class ConfigRepository {
+    private static final ObjectMapper mapper = new ObjectMapper(new YAMLFactory().disable(WRITE_DOC_START_MARKER).enable(MINIMIZE_QUOTES))
+            .configure(WRAP_ROOT_VALUE, true);
+    private static final Path SETTINGS_PATH = Paths.get(System.getProperty("user.home"), ".latte", "controller.yml");
+
+    private final RefreshEndpoint refreshEndpoint;
+
+    public void applyConfig(ControllerConfig controllerConfig) {
+        writeConfig(controllerConfig);
+        refreshConfig();
+    }
+
+    private void writeConfig(ControllerConfig controllerConfig) {
+        try {
+            mapper.writeValue(SETTINGS_PATH.toFile(), controllerConfig);
+        } catch (IOException e) {
+            log.error("Failed to write configs", e);
+            throw new IllegalStateException(e);
+        }
+    }
+
+    private void refreshConfig() {
+        refreshEndpoint.refresh();
+    }
+
+}
--- controller/backend/src/main/java/com/latte/controller/service/SettingsService.java
+++ controller/backend/src/main/java/com/latte/controller/service/SettingsService.java
@@ -1,35 +1,21 @@
 package com.latte.controller.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.latte.controller.config.ControllerConfig;
 import com.latte.controller.config.ControllerConfig.Settings;
+import com.latte.controller.repository.ConfigRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cloud.endpoint.RefreshEndpoint;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
-
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-import static com.fasterxml.jackson.databind.SerializationFeature.WRAP_ROOT_VALUE;
-import static com.fasterxml.jackson.dataformat.yaml.YAMLGenerator.Feature.MINIMIZE_QUOTES;
-import static com.fasterxml.jackson.dataformat.yaml.YAMLGenerator.Feature.WRITE_DOC_START_MARKER;
 
 /* TODO: prohibit config update when not registered */
 @RequiredArgsConstructor
 @Slf4j
 @Service
 public class SettingsService {
-    private static final ObjectMapper mapper = new ObjectMapper(new YAMLFactory().disable(WRITE_DOC_START_MARKER).enable(MINIMIZE_QUOTES))
-            .configure(WRAP_ROOT_VALUE, true);
-    private static final Path SETTINGS_PATH = Paths.get(System.getProperty("user.home"), ".latte", "controller.yml");
-
-    private final RefreshEndpoint refreshEndpoint;
     private final ControllerConfig controllerConfig;
     private final UserService userService;
+    private final ConfigRepository configRepository;
 
     public Mono<Settings> get() {
         return Mono.fromSupplier(() -> controllerConfig.getSettings().toPublicSettings());
@@ -62,25 +48,7 @@ public class SettingsService {
                 .settings(settings)
                 .build();
 
-        return Mono.fromRunnable(() -> applyConfig(newConfig));
-    }
-
-    private void applyConfig(ControllerConfig controllerConfig) {
-        writeConfig(controllerConfig);
-        refreshConfig();
-    }
-
-    private void writeConfig(ControllerConfig controllerConfig) {
-        try {
-            mapper.writeValue(SETTINGS_PATH.toFile(), controllerConfig);
-        } catch (IOException e) {
-            log.error("Failed to write settings", e);
-            throw new IllegalStateException(e);
-        }
-    }
-
-    private void refreshConfig() {
-        refreshEndpoint.refresh();
+        return Mono.fromRunnable(() -> configRepository.applyConfig(newConfig));
     }
 
     public Mono<Boolean> register(Settings settings) {
