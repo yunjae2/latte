13aecacac3c4b24edb4c28041369c7c62360a69a
Yunjae Lee <lyj7694@gmail.com>
2021-12-27 23:34:37 +0900

    con/be: Abort test when request fails
    In addition, failCounter is provided for custom abort logic.
    
    Signed-off-by: Yunjae Lee <lyj7694@gmail.com>

--- controller/backend/run/scripts/TestHelper.js
+++ controller/backend/run/scripts/TestHelper.js
@@ -1,18 +1,37 @@
+import { Counter } from "k6/metrics";
+
 export let handleSummary = (data) => {
 	return {
 		"summary.json": JSON.stringify(data, null, '\t')
 	}
 }
 
+export const failCounter = new Counter('fail_counter');
+
 export let options = {
 	discardResponseBodies: true,
+	thresholds: {
+		http_req_failed: [
+			{
+				threshold: 'rate == 0',
+				abortOnFail: true
+			}
+		],
+		fail_counter: [
+			{
+				threshold: 'count == 0',
+				abortOnFail: true,
+				delayAbortEval: '0s'
+			}
+		]
+	},
 	scenarios: {
 		contacts: {
 			executor: 'constant-arrival-rate',
-			rate: 50,
+			rate: 1,
 			duration: '10s',
-			preAllocatedVUs: 10,
-			maxVUs: 50,
+			preAllocatedVUs: 1,
+			maxVUs: 1,
 		},
 	},
 	summaryTrendStats: ['avg', 'min', 'med', 'max', 'p(50)', 'p(99)', 'p(99.9)', 'p(99.99)', 'p(100)', 'count'],
